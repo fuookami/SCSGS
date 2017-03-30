fc7725882dc6dedbafb5f52bef20f3e9e29f5199
fuookami <bagawolf2014@yahoo.co.jp>
2017-03-30 17:15:00 +0800

    项目创建

---
+++ NUAASwimmingAssociationVersion/NUAASwimmingAssociationVersion/main.cpp
@@ -0,0 +1,10 @@
+#include "nuaaswimmingassociationversion.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	NUAASwimmingAssociationVersion w;
+	w.show();
+	return a.exec();
+}
---
+++ ProvincialCompetitionVersion/ProvincialCompetitionVersion/main.cpp
@@ -0,0 +1,10 @@
+#include "provincialcompetitionversion.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	ProvincialCompetitionVersion w;
+	w.show();
+	return a.exec();
+}
