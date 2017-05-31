e15332ca1163bce9efe90d84600dd4eda1579fa0
fuookami <bagawolf2014@yahoo.co.jp>
2017-05-31 23:55:13 +0800

    构建基本数据结构

--- NUAASwimmingAssociationVersion/NUAASwimmingAssociationVersion/basic.h
+++ NUAASwimmingAssociationVersion/NUAASwimmingAssociationVersion/basic.h
@@ -1,49 +1,79 @@
 #pragma once
 
-#include <sstream>
-#include <fstream>
-#include <string>
+#include "Time.h"
 #include <vector>
 #include <array>
 
-using athlete = struct athleteStr
-{
-	std::string name;
-	int cla;
-	int team;
-	double grade;
-	std::string gradeStr;
-	bool hasGrade = false;
-	int thisRank;
-	int group;
-	int line;
-};
-
-bool operator<(const athlete &a, const athlete &b);
-bool operator==(const athlete &a, const athlete &b);
-
-using teamScore = struct teamScoreStr
-{
-	int team;
-	int score;
-};
-
-bool operator<(const teamScore &a, const teamScore &b);
-
-using game = struct gameStr
-{
-	std::string gameName;
-	int weight;
-	int currentGroup;
-	std::vector<std::array<athlete, 10>> groups;
-};
-
 namespace Setting
 {
-	static const std::vector<std::string> groupNames = { "第一组", "第二组", "第三组", "第四组", "第五组", "第六组", "第七组", "第八组", "第九组", "第十组", "第十一组", "第十二组", "第十三组", "第十四组", "第十五组", "第十六组", "第十七组", "第十八组", "第十九组", "第二十组" };
-	static const std::array<int, 8> rankScores = { 9, 7, 6, 5, 4, 3, 2, 1 };
+	static const std::vector<std::string> groupNames = { "第一组", "第二组", "第三组", "第四组", "第五组", "第六组", "第七组", "第八组", 
+		"第九组", "第十组", "第十一组", "第十二组", "第十三组", "第十四组", "第十五组", "第十六组", "第十七组", "第十八组", "第十九组", "第二十组" };
 	static const std::array<std::string, 3> dataNames = { "data/gameSetting.txt","data/teamsName.txt","data/game.txt" };
-	static const std::array<std::string, 4> classes = { "甲","乙","丙","丁" };
-	static const std::array<std::string, 4> classDisplay = { "甲","乙","丙","" };
 	static const unsigned LineNums = 10;
 }
+
+struct Athlete
+{
+	struct GameApplyInfo
+	{
+		unsigned int gameIndex;
+		unsigned int groupIndex;
+		unsigned int lineIndex;
+
+		Time time;
+		unsigned int groupRank;
+		unsigned int rank;
+		unsigned int getedGrade;
+	};
+
+	unsigned int uiCode;
+	std::string code;
+	unsigned int teamIndex;
+	unsigned int cla;
+	std::vector<GameApplyInfo> games;
+};
+
+struct Team
+{
+	struct GameApplyInfo
+	{
+		unsigned int gameIndex;
+		unsigned int athleteCode;
+		Athlete *pAthlete;
+		
+		unsigned int getedGrades;
+	};
+
+	unsigned int teamIndex;
+	unsigned int uiCode;
+	std::string code;
+	std::string name;
+
+	std::vector<GameApplyInfo> games;
+};
+
+struct Line
+{
+	unsigned int lineIndex;
+	unsigned int athleteCode;
+	Athlete *pAthlete;
+	Time time;
+	unsigned int groupRank;
+};
+
+struct Group
+{
+	unsigned int groupIndex;
+	std::array<Line, Setting::LineNums> lines;
+	bool hasOnceInput = false;
+};
+
+struct Game
+{
+	unsigned int gameIndex;
+	std::string gameName;
+	// 是否是团队项目
+	// 积分倍数
+	Time matchRecord;
+	std::vector<Group> groups;
+};
